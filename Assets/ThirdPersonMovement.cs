39e6f056d4100e54c93070c331fda94d787d0215
hugo <hugo.faugeroux@mmibordeaux.com>
2020-10-01 16:07:30 +0200

    character walk, jump and run

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -4,32 +4,78 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
-
-	public CharacterController controller;
-	public Transform cam;
-
-	public float speed = 6f;
-
-	public float turnSmoothTime = 0.1f;
+	//diplacement and camera
+	[SerializeField] private CharacterController controller;
+	[SerializeField] private Transform cam;
+	[SerializeField] private float wspeed = 6f;
+	[SerializeField] private float rspeed = 18f;
+	private float speed = 6f;
+	[SerializeField] private float turnSmoothTime = 0.1f;
 	float turnSmoothVelocity;
 
-    // Update is called once per frame
-    void Update()
-    {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+	//gravity and jump;
+	Vector3 velocity;
+	float gravity = -9.81f;
+	[SerializeField] private Transform groundCheck;
+	[SerializeField] private LayerMask groundMask;
+	[SerializeField] private float groundDistance = 0.2f;
+	[SerializeField] private float jumpHeight = 10.0f;
+	bool isGrounded;
 
-        if(direction.magnitude >= 0.1f)
-        {
-        	float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-        	float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-        	transform.rotation = Quaternion.Euler(0f, angle, 0f);
+	
 
-        	Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+	// Update is called once per frame
+	void Update()
+	{
+
+		Gravity();
+		Diplacement();
+		Run();
+		Jump();
+		print(isGrounded);
+
+		void Gravity(){
+			isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+			if (isGrounded && velocity.y < 0) {
+				velocity.y = -0.4f;
+			}
+			velocity.y += gravity * Time.deltaTime;
+			controller.Move(velocity * Time.deltaTime);
+		}
+
+		void Diplacement(){
+			float horizontal = Input.GetAxisRaw("Horizontal");
+			float vertical = Input.GetAxisRaw("Vertical");
+			Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized; 
+
+			if(direction.magnitude >= 0.1f)
+			{
+				float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+				float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+				transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+				Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+				controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+			}
+		}
+
+		void Run(){
+			if (Input.GetAxisRaw("Vertical") == 1.0f && Input.GetKey("left shift") && isGrounded) {
+				speed = rspeed;
+			}
+			else {
+				speed = wspeed;
+			}
+		}
+
+		void Jump() {
+			if (isGrounded && Input.GetKey("space")) {
+				velocity.y = jumpHeight;
+			}
+		}
 
 
-        	controller.Move(moveDir.normalized * speed * Time.deltaTime);
-        }
-    }
+	}
 }
